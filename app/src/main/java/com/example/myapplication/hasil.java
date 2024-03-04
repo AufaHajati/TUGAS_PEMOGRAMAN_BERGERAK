049e4657f80c9a6ac337de7dc065d1b4e5f2576d
AufaHajati <133089141+AufaHajati@users.noreply.github.com>
2024-03-04 20:36:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/hasil.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class hasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil);
+
+        // Mendapatkan nilai dari Intent
+        String result = getIntent().getStringExtra("result");
+
+        // Menampilkan hasil di TextView
+        TextView tvResult = findViewById(R.id.tv_result);
+        tvResult.setText(result);
+    }
+}
